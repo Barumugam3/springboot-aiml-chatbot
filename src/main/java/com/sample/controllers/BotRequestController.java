fe7fa0036f659fa0ba3b22ece71f45dacbe50890
srikanth-josyula <44222500+srikanth-josyula@users.noreply.github.com>
2020-05-17 09:02:56 +0530

    changes for REst Response

--- src/main/java/com/sample/controllers/BotRequestController.java
+++ src/main/java/com/sample/controllers/BotRequestController.java
@@ -1,5 +1,6 @@
 package com.sample.controllers;
 
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,10 +15,18 @@ public class BotRequestController {
 
 	@Autowired
 	ChatbotUtilService chatbot;
-	
+
+	@SuppressWarnings("unchecked")
 	@GetMapping(path = "/send/{message}")
-	public String sendReuest(@PathVariable String message) {
-		return chatbot.getBotResponse(message);
+	public JSONObject getBotResponse(@PathVariable String message) {
+		JSONObject responseObject = new JSONObject();
+		try {
+			responseObject.put("response", chatbot.getBotResponse(message));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return responseObject;
+
 	}
 
 }
